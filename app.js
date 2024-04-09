7362c5f603e645ab1ef2d32bb60cb9f2680bfa07
yuqihu1103 <hu.yuqi@northeastern.edu>
2024-04-09 16:41:16 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { connectToDatabase } from "./db/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+// Middleware for parsing JSON request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+//test route
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "Test success!" });
+});
+
+// Start your server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+connectToDatabase();
+export default app;
