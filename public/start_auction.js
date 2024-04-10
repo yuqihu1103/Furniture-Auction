130a41d86546994e990142f5b12a046c12c5fda5
Zhehao <x97589125@gmail.com>
2024-04-10 16:58:21 -0700

    start auction

--- public/start_auction.js
+++ public/start_auction.js
@@ -2,36 +2,34 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("form");
-    const add_furniture_success_div = document.getElementById("add_furniture_success");
+    const start_auction_success_div = document.getElementById("start_auction_success");
 
     form.addEventListener("submit", async (event) => {
         event.preventDefault();
 
         const formData = new FormData(form);
-        const type = formData.get("type");
-        const description = formData.get("description");
-        const picture_urls = formData.get("picture_urls");
-        const condition = formData.get("condition");
+        const furnitureId = formData.get("furnitureId");
+        const startPrice = formData.get("startPrice");;
 
         try {
-            const response = await fetch("/add_furniture", {
+            const response = await fetch("/start_auction", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ type, description, picture_urls, condition }),
+                body: JSON.stringify({ furnitureId, startPrice }),
             });
 
             const data = await response.json();
 
             if (response.ok) {
-                add_furniture_success_div.textContent = data.message;
+                start_auction_success_div.textContent = data.message;
             } else {
-                add_furniture_success_div.textContent = data.error;
+                start_auction_success_div.textContent = data.error;
             }
         } catch (error) {
             console.error("Error:", error);
-            add_furniture_success_div.textContent =
+            start_auction_success_div.textContent =
                 "An error occurred. Please try again later.";
         }
     });
--- routes/start_auction.js
+++ routes/start_auction.js
@@ -5,9 +5,8 @@ const router = express.Router();
 
 // Route to start an auction on furniture listed by the logged-in user
 router.post("/start_auction", (req, res) => {
-  const { furnitureId, startPrice } = req.body;
-  const sellerId = req.session.userId; // Retrieve seller's user ID from the session
-
+  let { furnitureId, startPrice } = req.body;
+  const userId = req.session.userId; // Retrieve seller's user ID from the session
   // Check if furniture ID is provided
   if (!furnitureId) {
     return res.status(400).json({ error: "Furniture ID is required" });
@@ -31,33 +30,45 @@ router.post("/start_auction", (req, res) => {
       return res.status(400).json({ error: "Furniture is already auctioned" });
     }
 
-    // Start a new auction
-    const startDate = new Date(); // Current date
-    const endDate = new Date();
-    endDate.setDate(startDate.getDate() + 3); // Set end date to 3 days after start date
-
-    const startAuctionQuery =
-      "INSERT INTO auction (start_date, end_date, start_price, seller_id, furniture_id, status, max_bid_price) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    connection.query(
-      startAuctionQuery,
-      [
-        startDate,
-        endDate,
-        startPrice,
-        sellerId,
-        furnitureId,
-        "pending",
-        startPrice,
-      ],
-      (error, results) => {
-        if (error) {
-          console.error("Error starting auction:", error);
-          return res.status(500).json({ error: "Internal server error" });
-        }
-
-        res.status(200).json({ message: "Auction started successfully" });
+    // get seller id first
+    const sellerIdQuery = `
+    SELECT seller_id
+    FROM seller
+    WHERE user_id = ? `;
+    connection.query(sellerIdQuery, [userId],  (error, results) => {
+      if (error) {
+        console.error("Error retrieving users:", error);
+        return res.status(500).json({ error: "Internal server error" });
       }
-    );
+      const sellerId = results[0].seller_id;
+      // Start a new auction
+      const startDate = new Date(); // Current date
+      const endDate = new Date();
+      endDate.setDate(startDate.getDate() + 3); // Set end date to 3 days after start date
+
+      const startAuctionQuery =
+          "INSERT INTO auction (start_date, end_date, start_price, seller_id, furniture_id, status, max_bid_price) VALUES (?, ?, ?, ?, ?, ?, ?)";
+      connection.query(
+          startAuctionQuery,
+          [
+            startDate,
+            endDate,
+            startPrice,
+            sellerId,
+            furnitureId,
+            "pending",
+            startPrice,
+          ],
+          (error, results) => {
+            if (error) {
+              console.error("Error starting auction:", error);
+              return res.status(500).json({ error: sellerId });
+            }
+
+            res.status(200).json({ message: "Auction started successfully" });
+          }
+      );
+    });
   });
 });
 
